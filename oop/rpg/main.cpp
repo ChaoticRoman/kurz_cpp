c6e406c3c9f9ce242ca58e852bfd528ad835f971
Roman Pavelka <roman.pavelka.asi@gmail.com>
2021-05-14 15:23:50 +0200

    Polymorphism downcasting example

--- oop/rpg/main.cpp
+++ oop/rpg/main.cpp
@@ -8,26 +8,34 @@
 using namespace std;
 
 void game() {
-    Player::printGameInfo(1);
+    //Player::printGameInfo(1);
 
     vector<shared_ptr<Player>> players;
-
     players.push_back(make_shared<Player>("Emil"));
     players.push_back(make_shared<Mage>("Cherryfuchs"));
 
     for (const auto &player: players)
     {
         player->printPlayerInfo();
+
+        auto mage = dynamic_pointer_cast<Mage>(player);
+        cout << player->name() << " is "
+             << (mage ? "" : "not ") << ("a mage.") << endl;
+
+        if (mage) mage->castMagic();
     }
 
-    // m.castMagic(); // TODO
+    for (const auto &player: players)
+    {
+        player->printPlayerInfo();
+    }
 
-    Player::printGameInfo(2);
+    //Player::printGameInfo(2);
 }
 
 int main() {
-    Player::printGameInfo(0);
+    //Player::printGameInfo(0);
     game();
-    Player::printGameInfo(3);
+    //Player::printGameInfo(3);
     return 0;
 }
